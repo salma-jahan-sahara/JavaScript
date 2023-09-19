da448dda373cde8516d8a838bad3738b470e4f3a
salma-jahan-sahara <salmajahansahara@gmail.com>
2023-09-19 10:03:25 +0600

    update

---
+++ 04 - Javascript Tutorial Continued/10-calculateTotal.js
@@ -0,0 +1,26 @@
+// Funtion, return, if, arrays, for loop
+const gas = [20, 40, 100, 30];
+const food = [10, 40, 50];
+
+function calculateTotal(arr) {
+  let total = 0;
+  for (let i = 0; i < arr.length; i++) {
+    total += arr[i];
+  }
+  if (total > 100) {
+    console.log(`You are spending more`);
+    return total;
+  }
+  console.log(`You are spending good`);
+  return total;
+}
+
+const gasTotal = calculateTotal(gas);
+const foodTotal = calculateTotal(food);
+const randomTotal = calculateTotal([200, 4000, 500, 1]);
+
+console.log({
+    gas: gasTotal,
+    food: foodTotal,
+    random: randomTotal,
+});
--- 04 - Javascript Tutorial Continued/script.js
+++ 04 - Javascript Tutorial Continued/script.js
@@ -70,7 +70,7 @@ const specificName = allNames.splice(2,1);
 console.log(specificName);
 console.log(allNames);
  */
-
+/* 
 // Array and for loop
 const names = ['sahara', 'prianka', 'sima'];
 const lastName = 'Sayed'
@@ -85,3 +85,89 @@ for(let i = 0; i< names.length;  i++)
   newArray.push(fullName);
 }
 console.log(newArray);
+ */
+/* Reference vs Value
+Primitive data type 
+String, Number, Boolean, Undefined, Null
+Arrays, Functions, Object = object
+typeof
+*/
+
+// When assigning primitive data type value to a variable any changes are made directly to that value, without affecting original value
+// When assigning non-primitive data type value to a variable is done by reference so any changes will affect all the reference
+/* 
+const number = 1;
+let number2 = number;
+number2 = 7;
+console.log(`First value ${number}`);
+console.log(`Second value ${number2}`);
+
+let person = {name : 'sahara'}
+let person2 = {...person};
+person2.name = 'sayed';
+console.log(`First person ${person.name}`);
+console.log(`First person ${person2.name}`); */
+
+/* 
+Null and undefined
+both represent "no value"
+undefined - "javscript can not find value for this"
+variable without value 
+missing function arguments
+missing object properties
+null - "developer set the value"
+*/
+
+/* let number = 20+null; 
+// 20 + 0
+console.log(number);
+let number2 = 20 + undefined;
+// 20 + 0
+console.log(number2);
+ */
+/* 
+Truthy & Falsy
+"", '',``, 0, -0, NaN, false, null, undefined
+*/
+/* const bool1 = true;
+const bool2 = 2 > 1;
+if (bool1) {
+  console.log(`works`);
+}
+if (bool2) {
+  console.log(`works`);
+} */
+
+/* const names = `sahara`;
+if (names) {
+  console.log(`Truthy`);
+} else {
+  console.log(`Falsy`);
+} */
+
+// unary operator - typeof
+let text = `sahara sayed`;
+console.log(typeof text);
+// operand
+
+let number = 5;
+let number2 = 2 + 5;
+// ternary operator
+// condition ? (runs if true) : (runs if false)
+// variable lookup
+const globalNumber = 5;
+function add(num1, num2) {
+  const globalNumber = 20;
+  const result = num1 + num2 + globalNumber;
+  function multiply() {
+    const globalNumber = 100;
+    const multiplyResult = result * globalNumber;
+    console.log(multiplyResult);
+  }
+  // console.log(multiplyResult); error
+  multiply();
+  return result;
+}
+console.log(add(3, 4));
+
+// call back function
