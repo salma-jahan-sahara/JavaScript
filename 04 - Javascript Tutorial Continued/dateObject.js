fe4cb7c753160fc9d49d4fd7d7a34f8b4374d2e2
salma-jahan-sahara <salmajahansahara@gmail.com>
2023-09-20 20:39:52 +0600

    advance js finished

---
+++ 04 - Javascript Tutorial Continued/dateObject.js
@@ -0,0 +1,42 @@
+const months = [
+  'January',
+  'February',
+  'March',
+  'April',
+  'May',
+  'June',
+  'July',
+  'August',
+  'September',
+  'October',
+  'November',
+  'December',
+];
+const days = [
+  'Saturday',
+  'Sunday',
+  'Monday',
+  'Tuesday',
+  'Wednesday',
+  'Thursday',
+  'Friday',
+];
+// current date
+// const date = new Date();
+// manual
+const date = new Date('1/12/2024');
+
+const month = date.getMonth();
+console.log(months[month]);
+
+const day = date.getDay();
+console.log(days[day]);
+
+console.log(date.getDate());
+console.log(date.getFullYear());
+
+const sentence = `${days[day]}, ${date.getDate()} ${
+  months[month]
+} ${date.getFullYear()}`;
+console.log(sentence);
+document.body.innerHTML = sentence;
---
+++ 04 - Javascript Tutorial Continued/mathObject.js
@@ -0,0 +1,14 @@
+/* 
+    Math - Standard built-in object - always available
+*/
+const division = (900/88);
+// const ans = Math.floor(division);
+// const ans = Math.ceil(division);
+// const ans = Math.sqrt(division);
+// const ans = Math.PI;
+const min = Math.min(2,7,8,9);
+const max = Math.max(2, 7, 8, 9);
+// const random = Math.random();
+// const random = Math.floor(Math.random() * 10);
+const random = Math.floor((Math.random() * 10)+1);
+console.log(random);
