50e196978bd753ee759f6eacb87dafe96727b3dc
salma-jahan-sahara <salmajahansahara@gmail.com>
2023-09-20 17:07:38 +0600

    problem solving arr method

--- 04 - Javascript Tutorial Continued/09-arrMethod.js
+++ 04 - Javascript Tutorial Continued/09-arrMethod.js
@@ -52,3 +52,54 @@ Multiple Videos
    }
 2. assign to survey and log
  */
+
+const updatedStudents = students.map(function (student) {
+  // console.log(students);
+   student.role = 'student';
+   return student;
+});
+console.log(updatedStudents);
+
+const highScores = students.filter(function (student) {
+  // if(student.score >= 80){
+  //    return student;
+  // }
+  // if(student.score >= 80) return student;
+   return student.score >= 80;
+});
+console.log(highScores);
+
+const specificId = students.find(function (student) {
+   return student.id === 2;
+});
+console.log(specificId);
+
+const averageScore =
+   students.reduce(function (scoresTotal, student) {
+      return scoresTotal + student.score;
+   }, 0) / students.length;
+console.log(averageScore);
+
+/* #### survey
+
+   1. list favorite subjects with reduce
+      {
+      english: 1
+      history: 1
+      math: 3
+      }
+   2. assign to survey and log
+ */
+
+   const survey = students.reduce(function(survey,student){
+      // console.log(student.favoriteSubject);
+      const favSubject = student.favoriteSubject;
+      if(survey[favSubject]){
+         survey[favSubject] = survey[favSubject]+1;
+      }
+      else{
+         survey[favSubject] = 1;
+      }
+      return survey;
+   },{})
+      console.log(survey);
---
+++ 04 - Javascript Tutorial Continued/data.js
@@ -0,0 +1,32 @@
+const students = [
+  {
+    id: 1,
+    name: 'peter',
+    score: 80,
+    favoriteSubject: 'math',
+  },
+  {
+    id: 2,
+    name: 'bob',
+    score: 70,
+    favoriteSubject: 'english',
+  },
+  {
+    id: 3,
+    name: 'susy',
+    score: 60,
+    favoriteSubject: 'history',
+  },
+  {
+    id: 4,
+    name: 'ana',
+    score: 90,
+    favoriteSubject: 'law',
+  },
+  {
+    id: 5,
+    name: 'banana',
+    score: 88,
+    favoriteSubject: 'math',
+  },
+];
